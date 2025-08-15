8d92c875f640375a361ff51c30191dbcb7c202e1
Massimiliano Pippi <mpippi@gmail.com>
2025-08-15 09:19:10 +0200

    use default error log

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: MIT
 
 use clap::Parser;
-use log::LevelFilter;
 use roy::{run, Args};
 
 #[tokio::main]
@@ -11,13 +10,7 @@ async fn main() -> anyhow::Result<()> {
 
     let mut builder = env_logger::Builder::new();
     let filter = args.verbosity.log_level_filter();
-
-    if filter == LevelFilter::Error && std::env::var("RUST_LOG").is_err() {
-        builder.filter_level(LevelFilter::Info);
-    } else {
-        builder.filter_level(filter);
-    }
-
+    builder.filter_level(filter);
     builder.init();
 
     run(args).await
